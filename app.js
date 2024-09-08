69e531d12d01f7104ff2096dc5ba599a4a588ed8
Orkhand4 <orkhand4@mail.ru>
2024-09-08 12:26:09 +0400

    fetch1st

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-fetch('https://jsonplaceholder.typicode.com/todos')
-.then((response) => response.json())
-.then((data) => console.log(data))
+// fetch('https://jsonplaceholder.typicode.com/todos')
+// .then((response) => response.json())
+// .then((data) => console.log(data))
+
+const response = fetch('https://jsonplaceholder.typicode.com/users');
+console.log(response);
