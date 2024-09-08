01de2d6307cc86c8894fb093eba84447af4c5039
Orkhand4 <orkhand4@mail.ru>
2024-09-08 12:37:41 +0400

    async

--- app.js
+++ app.js
@@ -5,9 +5,12 @@
 
 const demo = async() => {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  console.log(response);
+  const data = await response.json()
+  return data
 }
 
-demo()
+const users = demo()
+console.log(users);
+
 
 
