7718f7039ee729a9ecc1c35f85e1bdb49093746c
Orkhand4 <orkhand4@mail.ru>
2024-09-13 22:35:58 +0400

    practice

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-// fetch('https://jsonplaceholder.typicode.com/todos')
-// .then((response) => response.json())
-// .then((data) => console.log(data))
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+
+        return [i, j]
+      }
+    }
+  }
+};
 
 
-const demo = async() => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await response.json()
-  return data
-}
-
-const users = demo()
-console.log(users);
-
-
-
+console.log(twoSum([2, 3, 4, 6], 10));
